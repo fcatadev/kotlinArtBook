1d569853568f42d9d3e44b83caefcfec806c48ca
trainerdev <fatihcane0696@outlook.com>
2022-11-10 18:28:27 +0300

    menu operation

--- app/src/main/java/com/fcadev/kotlinartbook/MainActivity.kt
+++ app/src/main/java/com/fcadev/kotlinartbook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.fcadev.kotlinartbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.fcadev.kotlinartbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,4 +17,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //inflater - xml kod bağlama işleminde her zaman inflater kullanılır.
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.menuId){
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
