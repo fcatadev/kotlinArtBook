3d293bf9e7f2907c82282a86248b8de6e25c7bdc
trainerdev <fatihcane0696@outlook.com>
2022-11-10 18:45:52 +0300

    request permission

--- app/src/main/java/com/fcadev/kotlinartbook/ArtActivity.kt
+++ app/src/main/java/com/fcadev/kotlinartbook/ArtActivity.kt
@@ -1,9 +1,16 @@
 package com.fcadev.kotlinartbook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.fcadev.kotlinartbook.databinding.ActivityArtBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ArtActivity : AppCompatActivity() {
 
@@ -18,6 +25,24 @@ class ArtActivity : AppCompatActivity() {
 
     fun saveButtonClicked(view: View){}
 
-    fun selectImage(view: View){}
+    fun selectImage(view: View){
+
+        // Galariye erişim izin işlemleri
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //neden izin istediğimizi göstermek
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", View.OnClickListener {
+                    //izin istemek
+                }).show()
+            }else{
+                //izin istemek
+            }
+        }else{
+            //Galeriye git
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        }
+
+    }
 
 }
