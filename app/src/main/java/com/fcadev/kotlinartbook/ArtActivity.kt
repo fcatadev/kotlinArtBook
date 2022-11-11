0fba8f9324b2e921230acdea17bb730ef430aa88
trainerdev <fatihcane0696@outlook.com>
2022-11-11 16:11:52 +0300

    create SQLite database

--- app/src/main/java/com/fcadev/kotlinartbook/ArtActivity.kt
+++ app/src/main/java/com/fcadev/kotlinartbook/ArtActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.fcadev.kotlinartbook.databinding.ActivityArtBinding
 import com.google.android.material.snackbar.Snackbar
+import java.io.ByteArrayOutputStream
 
 class ArtActivity : AppCompatActivity() {
 
@@ -40,6 +41,30 @@ class ArtActivity : AppCompatActivity() {
 
         if (selectedBitmap != null){
             val smallBitmap = makeSmallerBitmap(selectedBitmap!!, 300)
+
+            val outputStream = ByteArrayOutputStream()
+            smallBitmap.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+            val byteArr = outputStream.toByteArray()
+
+            try {
+                val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null)
+                database.execSQL("CREATE TABLE IF NOT EXISTS arts (id INTEGER PRIMARY KEY, artname VARCHAR, artistname VARCHAR, year VARCHAR, image BLOB)")
+
+                val sqlString = "INSERT INTO arts (artname, artistname, year, image) VALUES (?, ?, ?, ?)"
+                val statement = database.compileStatement(sqlString)
+                statement.bindString(1, artName)
+                statement.bindString(2, artistName)
+                statement.bindString(3, year)
+                statement.bindBlob(4, byteArr)
+                statement.execute()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+            val intent = Intent(this@ArtActivity, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) //bundan önce açık ne kadar activity varsa hepsini kapat maine dön demek.
+            startActivity(intent)
+
         }
     }
 
