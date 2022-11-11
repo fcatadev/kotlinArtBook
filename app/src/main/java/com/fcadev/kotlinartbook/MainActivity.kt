2e72505397f58df335e0f7dcb965692ea5a5a348
trainerdev <fatihcane0696@outlook.com>
2022-11-11 16:29:22 +0300

    recycler view operation

--- app/src/main/java/com/fcadev/kotlinartbook/MainActivity.kt
+++ app/src/main/java/com/fcadev/kotlinartbook/MainActivity.kt
@@ -5,17 +5,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fcadev.kotlinartbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private lateinit var artList: ArrayList<Art>
+    private lateinit var artAdapter: ArtAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        artList = ArrayList<Art>()
+
+        artAdapter = ArtAdapter(artList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = artAdapter
+
+        try {
+            val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null)
+
+            val cursor = database.rawQuery("SELECT * FROM arts", null)
+            val artNameIndex = cursor.getColumnIndex("artname")
+            val artIdIndex = cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()){
+                val name = cursor.getString(artNameIndex)
+                val id = cursor.getInt(artIdIndex)
+
+                val art = Art(name, id)
+                artList.add(art)
+            }
+
+            artAdapter.notifyDataSetChanged()
+
+            cursor.close()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
